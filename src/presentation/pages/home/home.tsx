231e83d1e78c442b0aa388d96f4185d0a2e48943
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-11-10 15:56:28 -0300

    feat: add form-container

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,9 +1,17 @@
-import { Table } from '@/presentation/components';
+import { Table, TextField } from '@/presentation/components';
+import { FormContainer, FormControl } from './home-styles';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
+      <FormContainer>
+        <FormControl>
+          <TextField name="name" />
+          <TextField name="email" />
+          <TextField name="phoneNumber" />
+        </FormControl>
+      </FormContainer>
       <Table></Table>
     </div>
   );
