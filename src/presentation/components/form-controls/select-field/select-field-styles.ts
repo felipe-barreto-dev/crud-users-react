d6b11d170ced669f48eb62207cb6b90375d4cc1b
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-11-13 16:41:23 -0300

    styles: add font-size and spacement

--- src/presentation/components/form-controls/select-field/select-field-styles.ts
+++ src/presentation/components/form-controls/select-field/select-field-styles.ts
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
 
-export const Select = styled.select``;
-export const Option = styled.option``;
+export const Select = styled.select`
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+`;
+
+export const Option = styled.option`
+  padding: 5px;
+`;
