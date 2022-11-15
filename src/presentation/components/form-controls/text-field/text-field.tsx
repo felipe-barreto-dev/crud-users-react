88dad0d837fc4670b9fb9242ace8eb1eb6ed727f
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-11-14 23:36:39 -0300

    feat: add value and setValue prop types

--- src/presentation/components/form-controls/text-field/text-field.tsx
+++ src/presentation/components/form-controls/text-field/text-field.tsx
@@ -3,10 +3,12 @@ import { Input } from './text-field-styles';
 
 type TextFieldProps = {
   name: string;
+  value: string;
+  setValue: (value: string) => void;
 };
 
-const TextField: React.FC<TextFieldProps> = ({ name }) => {
-  return <Input name={name} />;
+const TextField: React.FC<TextFieldProps> = ({ name, value, setValue }) => {
+  return <Input value={value} onChange={(e) => setValue(e.target.value)} name={name} />;
 };
 
 export default TextField;
